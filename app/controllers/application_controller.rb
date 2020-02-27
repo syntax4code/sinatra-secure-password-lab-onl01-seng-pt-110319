b4a89dd9d5477ddab04a81e9db5dc6c4fe32b4de
Warren Harper II <warrenharper2@att.net>
2020-02-27 01:34:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,17 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    if params[:username].empty?
+      redirect to '/failure'
+    end
 
+    user = User.new(:username => params[:username], :password => params[:password])
+
+		if user.save
+			redirect '/login'
+		else
+			redirect '/failure'
+		end
   end
 
   get '/account' do
