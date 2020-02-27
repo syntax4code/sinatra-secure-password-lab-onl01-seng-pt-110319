dc32e675f5b7dd82ea6c06ac61299ff14a24eec9
Warren Harper II <warrenharper2@att.net>
2020-02-27 01:36:33 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,6 +52,14 @@ class ApplicationController < Sinatra::Base
 		end
   end
 
+  get "/success" do
+		if logged_in?
+			erb :success
+		else
+			redirect "/login"
+		end
+  end
+
   get "/failure" do
     erb :failure
   end
